3a59d89e670de044f3f54b32f218e7e000efd46c
Jared Silver <jrsilver88@gmail.com>
2020-04-13 20:28:55 -0700

    nps install spotify, moment, axios. started variables in liri.js

--- liri.js
+++ liri.js
@@ -3,3 +3,30 @@ require("dotenv").config();
 var keys = require("./keys.js");
 
 var spotify = new Spotify(keys.spotify);
+var axios = require("axios");
+var moment = require("moment");
+var spotifys = require('node-spotify-api');
+var defaultMovie = "Independence Day";
+
+var action = process.argv[2];
+var value = process.argv[3];
+
+switch (action) {
+    case "concert-this":
+        getBands(value)
+        break;
+    case "spotify-this-song":
+        getSongs(value)
+        break;
+    case "movie-this":
+        if (value == "") {
+            value = defaultMovie;
+        }
+        getMovies(value)
+        break;
+    case "dow-what-it-says":
+        doWhatItSays()
+        break;
+    default:
+        break;
+}
